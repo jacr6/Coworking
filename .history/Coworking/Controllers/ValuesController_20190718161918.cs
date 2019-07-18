5afe6e605e251f241b3f3221276f769a32474af1
jacr6 <jose.cuevas.cv@gmail.com>
2019-07-18 16:51:20 -0500

    prueba 03

--- .history/Coworking/Controllers/ValuesController_20190718161918.cs
+++ .history/Coworking/Controllers/ValuesController_20190718161918.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
- 
+
 namespace Coworking.Controllers
 {
     [Route("api/[controller]")]
